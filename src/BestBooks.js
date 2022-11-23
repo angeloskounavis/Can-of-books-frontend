b8d8dfc07c99f0351d0249a4a7c165e2d0650275
Angelos <kounavisangelo@gmail.com>
2022-11-22 19:02:52 -0500

    frontend

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 import Carousel from 'react-bootstrap/Carousel';
+import { Button, Container} from 'react-bootstrap';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -55,6 +55,9 @@ class BestBooks extends React.Component {
             <Carousel>
               {carouselItems}
             </Carousel>
+            <Button type="button"
+              class="btn btn-primary">Add a book
+            </Button>
           </Container>
         ) : (
           <h3>No Books Found :(</h3>
