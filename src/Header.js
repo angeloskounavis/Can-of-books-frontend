f76fd3641e6e35b0feb692908c490a615ad9f9c8
Angelos <kounavisangelo@gmail.com>
2022-11-29 19:54:00 -0500

    login and logout btns

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import AuthButtons from './AuthButtons';
 import './Header.css';
 
 class Header extends React.Component {
@@ -10,6 +11,8 @@ class Header extends React.Component {
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
         <NavItem><Link to="/about" className="nav-link">About</Link></NavItem>
+        <NavItem><AuthButtons/></NavItem>
+        
       </Navbar>
     );
   }
---
+++ src/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Welcome extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Please login to see our collection of books</h1>
+        
+      </>
+    );
+  }
+}
+
+export default Welcome;
